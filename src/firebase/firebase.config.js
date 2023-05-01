b94c4b4ad9a692f152737312284b37aa0d60d783
Masum Rahman <masumrahman73@gmail.com>
2023-05-01 21:41:45 +0600

    router tailwind daisy-ui firebase installed and firebase config file added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBCGZpdCCdXgpk9jhi0kp45ANiTiomgnlU",
+  authDomain: "foodaholic-22ee5.firebaseapp.com",
+  projectId: "foodaholic-22ee5",
+  storageBucket: "foodaholic-22ee5.appspot.com",
+  messagingSenderId: "153488350854",
+  appId: "1:153488350854:web:fd5ba4f0d77d40fda8bb07"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
