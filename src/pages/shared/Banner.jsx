ea8cceb5e5bf0312d37ce557ca70f822e3d9ff07
Masum Rahman <masumrahman73@gmail.com>
2023-05-02 14:21:12 +0600

    banner primarily done

--- src/pages/shared/Banner.jsx
+++ src/pages/shared/Banner.jsx
@@ -1,10 +1,49 @@
 import React from 'react';
+import { Carousel, Container} from 'react-bootstrap';
 
 const Banner = () => {
     return (
-        <div>
-            <h2>Banner</h2>
-        </div>
+        <>
+            <Carousel>
+                <Carousel.Item>
+                    <img style={{objectFit: 'cover' , width: '100vw', height: '80vh'}}
+                        className="d-block w-100"
+                        src="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img style={{objectFit: 'cover' , width: '100vw', height: '80vh'}}
+                        className="d-block w-100"
+                        src="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                        alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img style={{objectFit: 'cover' , width: '100vw', height: '80vh'}}
+                        className="d-block w-100"
+                        src="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Third slide label</h3>
+                        <p>
+                            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                        </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        </>
     );
 };
 
