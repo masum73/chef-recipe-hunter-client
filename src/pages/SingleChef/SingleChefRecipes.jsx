91ef5926b58fac7fbc3ddb01d6201b5bdd2f2819
Masum Rahman <masumrahman73@gmail.com>
2023-05-04 03:25:33 +0600

    rating start installed and displayed

--- src/pages/SingleChef/SingleChefRecipes.jsx
+++ src/pages/SingleChef/SingleChefRecipes.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Button, Card } from 'react-bootstrap';
+import { Rating } from '@smastrom/react-rating';
+import '@smastrom/react-rating/style.css'
 
 const SingleChefRecipes = ({ recipe }) => {
     const { recipe_name,cooking_method, rating  } = recipe;
@@ -10,7 +12,9 @@ const SingleChefRecipes = ({ recipe }) => {
                 <Card.Title>{recipe_name}</Card.Title>
                 <Card.Text>
                     Cooking Method: {cooking_method}
-                    Rating: {rating}
+                    <Rating 
+                    style={{ maxWidth: 100, marginTop: '1rem' }} 
+                    value={Math.round(rating || 0)} readOnly />
                 </Card.Text>
                 <Button>Add to Favorite</Button>
             </Card.Body>
